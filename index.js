347b144e6f60d7f8dd42a8775ca5ff0c0626c159
Chris <Kirch2@gmail.com>
2021-07-22 20:16:34 -0400

    commit tree 3c4e140a3be923384c0a3ea2a9a6bf9bd3b80af0

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
+  const newTutorials = tutorials.map((line) => makeLineTitleCase(line));
+
+  return newTutorials;
+};
+
+function makeWordTitleCase(w) {
+  return w.charAt(0).toUpperCase() + w.slice(1);
 }
+
+function makeLineTitleCase(str) {
+  return str
+    .split(" ")
+    .map((w) => makeWordTitleCase(w))
+    .join(" ");
+}
+
+// console.log('running')
+//const word = 'what does the this keyword mean?';
+// console.log(word)
+// console.log(word.split(" ").map(w => w.charAt(0).toUpperCase() + w.slice(1)))
+// console.log(word.split(" ").map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(" "))
+// console.log(makeTitleCase(word));
+
+// console.log(makeWordTitleCase("car"));
